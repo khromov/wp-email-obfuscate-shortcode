e78110c96e4815acd096e0867de42a8286bc9cfb
Stanislav Khromov <stanislav.khromov@gmail.com>
2013-06-05 16:25:51 +0200

    Initial commit

---
+++ email-obfuscate-shortcode.php
@@ -0,0 +1,189 @@
+<?php
+/*
+Plugin Name: Email Obfuscate Shortcode
+Plugin URI: http://wordpress.org/extend/plugins/email-obfuscate-shortcode
+Description: No more spam! Example usage: [email-obfuscate email="bob@company.com"] -- See plugin site for more examples.
+Version: 1.3
+Author: khromov
+Author URI: http://khromov.wordpress.com
+License: GPL2
+*/
+
+/**
+ * Debug flag
+ **/
+define('EOS_DEBUG', false);
+
+/**
+ * Register activation hook
+ **/
+register_activation_hook( __FILE__, array( 'EOS', 'activate' ) );
+
+/**
+ * Translation support
+ **/
+load_plugin_textdomain('email-obfuscate-shortcode', false, basename( dirname( __FILE__ ) ) . '/languages' );
+
+/**
+ * Register the shortcode
+ **/
+add_shortcode('email-obfuscate', array('EOS', 'obfuscate_shortcode'));
+
+/**
+ * Function for calling EOS from other plugins.
+ * 
+ * This is function is callable in any of your plugins.
+ * 
+ * Example of array to pass to args:
+ * 
+ * Simple:
+ * array
+ *  (
+ *      'email' => 'bob@company.com'
+ *  )
+ * 
+ * All args:
+ * array
+ *  (
+ *      'email' => 'bob@company.com',
+ *      'linkable' => 1,
+ *      'link_title' => 'Click here to email Bob!',
+ *      'use_htmlentities' => 1,
+ *      'use_noscript_fallback' => 1,
+ *      'noscript_message' => 'Please enable JavaScript to see this field'
+ *   )
+ * 
+ **/
+function eos_obfuscate($args)
+{
+    return EOS::obfuscate_shortcode($args);
+}
+
+/**
+ * Main class
+ **/
+class EOS
+{
+    /**
+     * Plugin activation function
+     **/
+    static function activate()
+    {
+        //Check for required functions
+        if(!function_exists('mb_convert_encoding') || !function_exists('mb_detect_encoding'))
+            die(__('You need to install the PHP Multibyte String (mbstring) extension to use this plugin.'));
+    }
+    
+    /**
+     * Obfuscation routine
+     **/
+    static function obfuscate_shortcode($args)
+    {
+        //Get values from shortcode and put them in local variables
+        extract
+        (
+            shortcode_atts
+            (
+                array
+                (
+        	       'email' => false,
+                   'linkable' => true, //0 if you want to store phones, names or other hidden information instead of emails 
+                   'link_title' => "",
+                   'use_htmlentities' => true,
+                   'use_noscript_fallback' => true,
+                   'noscript_message' => __("Please enable JavaScript to see this field.", "email-obfuscate-shortcode")
+                ),
+                $args
+            )
+        );
+        
+        if(!$email)
+            return __("You have not entered an email address for this shortcode.", "email-obfuscate-shortcode");
+        else
+        {        
+            //Init return variable
+            $ret = $email;
+            
+            //Encode as htmlentities
+            if($use_htmlentities)
+                $ret = EOS::html_entities_all($ret);
+            
+            //Wrap in mailto: link
+            if($linkable)
+                $ret = "<a href=\"mailto:{$ret}\">" . ($link_title=='' ? $ret : $link_title) ." </a>";
+            
+            //Convert to JS snippet
+            $ret = EOS::safe_text($ret);
+                
+            //Add noscript fallback
+            if($use_noscript_fallback)
+                $ret .= "<noscript>{$noscript_message}</noscript>"; 
+            
+            if(EOS_DEBUG)
+            {
+                $ret .= "
+                            <div class=\"eos_debug\">
+                                --- EOS debug info: --- <br />
+                                Raw email string: {$email} <br/>
+                                Linkable: {$linkable} <br/>
+                                Link title: {$link_title} <br/>
+                                noscript fallback: {$use_noscript_fallback}<br/>
+                                noscript message: {$noscript_message}<br/>
+                                --- End of EOS debug info ---
+                            </div>
+                        ";      
+            }
+            return $ret;
+        }
+    }
+    
+    /**
+     * Encodes every character in $text into its numeric html representation.
+     * http://stackoverflow.com/questions/3005116/how-to-convert-all-characters-to-their-html-entity-equivalent-using-php/3005240
+     */
+    static function html_entities_all($text)
+    {
+        $text = mb_convert_encoding($text , 'UTF-32', 'UTF-8');
+        $t = unpack("N*", $text);
+        $t = array_map(array('EOS', 'html_entities_closure_wrap'), $t);
+        
+        return implode("", $t);
+    }
+    
+    //For PHP <5.3 support.
+    static function html_entities_closure_wrap($n)
+    {
+    	return "&#$n;";
+    }
+    
+    /**
+     * safe_text() obfuscator function
+     * http://khromov.wordpress.com/2011/10/04/php-function-for-scrambling-e-mail-addressesphone-numbers-using-javascript/
+     **/
+    static function safe_text($text)
+    {
+        //Check if text is UTF-8 and decode if it is
+        if(mb_detect_encoding($text, 'UTF-8', true))
+                $text = utf8_decode($text);
+        
+        //Create the obfuscation array
+        $chars = str_split($text);
+    
+        $enc[] = rand(0,255);
+    
+        foreach($chars as $char)
+            $enc[] = ord($char)-$enc[sizeof($enc)-1];
+        
+        $finished_array = join(',',$enc); 
+        
+        $ret = "<script type=\"text/javascript\">
+                    var t=[{$finished_array}];
+                    for (var i=1; i<t.length; i++)
+                    {
+                        document.write(String.fromCharCode(t[i]+t[i-1])); 
+                    }
+                </script>";
+                
+        return $ret;   
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,107 @@
+=== Email Obfuscate Shortcode ===
+Contributors: khromov
+Tags: email, obfuscator, email obfuscator, spam stopper
+Requires at least: 3.0
+Tested up to: 3.5
+Stable tag: 1.3
+License: GPL2
+
+Obfuscate your email address or other sensitive information with a shortcode to prevent spam and automated harvesting of data.
+
+== Description ==
+Obfuscate your email address or other sensitive information with a shortcode to prevent harvesting of your data.
+
+**Usage**
+
+*Basic usage*
+
+[email-obfuscate email="bob@example.com"]
+
+*Setting custom link title*
+
+[email-obfuscate email="bob@example.com" link_title="Email Bob!"]
+
+*Using every available setting (this example shows their default values)*
+
+[email-obfuscate email="bob@example.com" linkable="1" link_title="" use_htmlentities="1" use_noscript_fallback="1" noscript_message="Please enable JavaScript to see this field."]
+
+**Usage from a template or plugin**
+
+Below is a snippet that you can use in any template or plugin to apply the same obfuscation to an email as using the shortcode:
+
+if(function_exists('eos_obfuscate'))
+{
+  echo eos_obfuscate(array('email' => $email, 'link_title' => 'Email Bob!'));
+}
+else 
+{
+  echo $email;
+}
+    
+If the plugin is not enabled, the email address will just pass through and output in cleartext.
+
+== Requirements ==
+* PHP 5.3 or higher
+* mbstring extension
+
+== Translations ==
+* Swedish
+
+== Installation ==
+1. Upload the `email-obfuscate-shortcode` folder to `/wp-content/plugins/`
+2. Activate the plugin (Email Obfuscate Shortcode) through the 'Plugins' menu in WordPress
+3. Use the shortcode in any post, page or custom post type.
+
+== Frequently Asked Questions ==
+
+= How does this plugin prevent my email address or other personal information from being harvested? =
+
+Email Obfuscate Shortcode converts your email address into JavaScript-based snippet. This makes the address unreadable to the vast majority
+of email harvesting techniques.
+
+= What browsers is this plugin compatible with? =
+Internet Explorer 6 and up, any Firefox, Chrome, Safari or Opera version.
+
+= How do you obfuscate information other than email addresses? =
+
+To obfuscate phone numbers, names and other sensitive information, pass the parameter linkable="0". This will print anything you
+put in the "email" field as text onto your page, fully protected.
+
+= Will people without JavaScript see my email address? =
+
+People without JavaScript will see a placeholder message urging them to enable JavaScript. The current placeholder message reads:
+"Please enable JavaScript to see this field."
+
+You can customize this message by passing the noscript_message="" parameter to the shortcode.
+
+= Is the solution bulletproof? =
+
+This protection has been proven very effective. However it is theoretically possible to harvest the email address if you run a real browser that resolves javascript (Selenium, Mechanical Turk workers etc.)
+
+== Screenshots ==
+
+1. Shortcode example in editor
+2. Obfuscated JavaScript code as rendered to page
+
+== Changelog ==
+= 1.3 =
+* Fixed compatibility issue with PHP 5.2 (5.3 is still recommended)
+* Fixed a bug that made use_htmlentities setting not work. 
+
+= 1.2 =
+* Minor documentation fixes
+
+= 1.1 =
+* Changed Plugin URI to the plugin page at Wordpress.org
+
+= 1.0 =
+* Initial release
+
+== Upgrade Notice ==
+= 1.0 =
+Initial release
+
+== TODO ==
+* Better noscript_fallback. Enable people without JavaScript to see the email address using technique to inject empty spans via css.
+(Example: bob@<span style="display:none">randomtext</span>company.com
+Reference: kolla på http://wordpress.org/extend/plugins/obfuscate-email/other_notes/
