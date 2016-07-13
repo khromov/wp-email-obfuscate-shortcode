59d23b203038664bf8dccebc15626ee722e77cdc
Stanislav Khromov <stanislav+github@khromov.se>
2016-07-13 09:45:25 +0200

    Test again

--- email-obfuscate-shortcode.php
+++ email-obfuscate-shortcode.php
@@ -3,6 +3,7 @@
 Plugin Name: Email Obfuscate Shortcode
 Plugin URI: http://wordpress.org/extend/plugins/email-obfuscate-shortcode
 Description: No more spam! Example usage: [email-obfuscate email="bob@company.com"] -- See plugin site for more examples.
+Version: 2.1.0
 Author: khromov
 Author URI: http://khromov.wordpress.com
 License: GPL2
