4647a332c97e9e40fe00a129a4ee8dd80467933d
Stanislav Khromov <stanislav+github@khromov.se>
2016-07-13 10:32:50 +0200

    Hopefully working test now :)

--- tests/tests.php
+++ tests/tests.php
@@ -5,12 +5,17 @@
 class EOS_Test extends PHPUnit_Framework_TestCase
 {
     public function testTest() {
+
+        /*
         $expected = 6;
 
         $test = function (int ...$ints) {
             return array_sum($ints);
         };
+        */
+        $test = 2+3;
+        $expected = 5;
 
-        $this->assertEquals($expected, $test(1,2,3));
+        $this->assertEquals($expected, $test);
     }
 }
