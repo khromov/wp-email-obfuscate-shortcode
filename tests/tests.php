41accdd4d69be55de3fdef57fca155a68d3361fd
Stanislav Khromov <stanislav+github@khromov.se>
2016-07-13 10:09:22 +0200

    Test invalid PHP 5.2 syntax + fix travis step

--- tests/tests.php
+++ tests/tests.php
@@ -2,15 +2,14 @@
 //TODO: Perform actual tests
 //require_once "../email-obfuscate-shortcode.php";
 
-function test_me() {
-    return 5;
-}
-
 class EOS_Test extends PHPUnit_Framework_TestCase
 {
     // test the talk method
     public function testTest() {
         $expected = 5;
-        $this->assertEquals($expected, test_me());
+        $test = function() {
+            return 5;
+        };
+        $this->assertEquals($expected, $test());
     }
 }
